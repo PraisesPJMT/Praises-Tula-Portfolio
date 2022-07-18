916fdd09bacee235985a68442c47230faf7eab7c
PraisesPJMT <praisesmusa@outlook.com>
2022-07-18 16:27:32 +0100

    Added some mobile menu features

---
+++ main.js
@@ -0,0 +1,34 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuClose = document.querySelector('.menu-close');
+const header = document.querySelector('header');
+const navigationBar = document.querySelector('nav');
+
+// console.log(navigationBar);
+
+menuIcon.addEventListener('click', () => {
+  header.style.height = '100vh';
+  header.style.background = 'url("images/Group_98.svg") no-repeat, url("images/Group_50.svg") no-repeat 80% 10%, url("images/Group_50.svg") no-repeat 20% 80%, url("images/Mask_Group.svg") no-repeat 0% 100%';
+  header.style.backgroundSize = '35.5%, 15%, 15%, 100%';
+  header.style.display = 'block';
+
+  menuIcon.style.display = 'none';
+  menuClose.style.display = 'inline';
+  menuClose.style.float = 'right';
+  menuClose.style.transform = 'scale(1.6)';
+
+  navigationBar.style.visibility = 'visible';
+  navigationBar.style.flexDirection = 'column';
+  navigationBar.style.display = 'flex';
+});
+
+menuClose.addEventListener('click', () => {
+  header.style.height = '100px';
+  header.style.background = 'url("images/image_geometry_1.svg") no-repeat';
+  header.style.backgroundSize = '35.5%';
+
+  menuClose.style.display = 'none';
+  menuIcon.style.float = 'right';
+  menuIcon.style.display = 'inline';
+
+  navigationBar.style.display = 'none';
+});
