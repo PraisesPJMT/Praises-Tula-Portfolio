eb0535f6fb568744c5d4ce20bf5fe7d09d81acf7
PraisesPJMT <praisesmusa@outlook.com>
2022-07-22 09:02:37 +0100

    Add locale storage feature for storing form inputs

--- javascript/formvalidation.js
+++ javascript/formvalidation.js
@@ -1,14 +1,29 @@
 const contactForm = document.getElementById('contact-form');
+const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
 const errorMessage = document.querySelector('.error-message');
+const storedFormData = JSON.parse(localStorage.getItem('formData'));
 
-function validateEmail(form) {
+window.addEventListener('load', () => {
+  if (Object.keys(storedFormData).length) {
+    nameInput.value = storedFormData.name;
+    emailInput.value = storedFormData.email;
+    messageInput.value = storedFormData.message;
+  }
+});
+
+contactForm.addEventListener('submit', (form) => {
   if (emailInput.value !== emailInput.value.toLowerCase()) {
     form.preventDefault();
     errorMessage.style.display = 'block';
   } else {
     errorMessage.style.display = 'none';
+    const formData = {
+      name: nameInput.value,
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
   }
-}
-
-contactForm.addEventListener('submit', validateEmail);
+});
